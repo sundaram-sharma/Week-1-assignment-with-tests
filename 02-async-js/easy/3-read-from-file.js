9bb331b5ac3f7cea5141a2a4aa0b3ce140e0c6dc
Sundaram Sharma <sundaram-sharma@hotmail.com>
2023-07-03 23:33:41 +0530

    work done 2

--- 02-async-js/easy/3-read-from-file.js
+++ 02-async-js/easy/3-read-from-file.js
@@ -1,6 +1,5 @@
-
-
-const fs = require('fs');
-
-
-fs.readFile('a.txt','utf8', printFile);
+const fs = require('fs')
+fs.readFile('a.txt', (err, inputD) => {
+   if (err) throw err;
+      console.log(inputD.toString());
+})
